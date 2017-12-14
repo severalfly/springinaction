993c6bc2f827ac1406d47464b16e1fc1604a017a
severalfly <zhangsmile90@gmail.com>
2017-12-14 20:27:55 +0800

    base web mvc

--- src/main/java/com/leon/spittr/config/RootConfig.java
+++ src/main/java/com/leon/spittr/config/RootConfig.java
@@ -1,8 +1,13 @@
 package com.leon.spittr.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@ComponentScan(basePackages = { "com.leon.spittr" }, excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
 public class RootConfig
 {
 
