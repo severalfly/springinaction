8649374e8f1dee6b2f830cd3bbff5efb68ae8279
severalfly <zhangsmile90@gmail.com>
2017-12-19 22:40:01 +0800

    now

--- src/main/java/com/leon/spittr/data/SpittleRepository.java
+++ src/main/java/com/leon/spittr/data/SpittleRepository.java
@@ -2,9 +2,12 @@ package com.leon.spittr.data;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+
 import com.leon.spittr.Spittle;
 
 public interface SpittleRepository
 {
+	@Bean
 	List<Spittle> findSpittles(long max, int count);
 }
